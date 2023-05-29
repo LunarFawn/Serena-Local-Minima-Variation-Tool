5131b89ce90d493f1a33c9b46cd0c083a613baf7
Jennifer Pearl <jenpearl5@gmail.com>
2023-05-28 20:09:53 -0700

    stuff

---
+++ tests/test_ensemble_variation.py
@@ -0,0 +1,12 @@
+"""
+pytest for ensemble variation
+"""
+
+import pytest
+
+import serena.ensemble_variation as ev
+import serena.structures as ser_structs
+import serena.nupack4_sara2_extension as nupack_extension
+
+
+
