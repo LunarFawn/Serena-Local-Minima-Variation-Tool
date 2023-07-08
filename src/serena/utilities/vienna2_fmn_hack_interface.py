74c6d50ce1211d0624d809e9961202b7ea50e07c
Jennifer Pearl <jenpearl5@gmail.com>
2023-07-08 10:31:02 -0700

    fmn hack interace ready

--- src/serena/utilities/vienna2_fmn_hack_interface.py
+++ src/serena/utilities/vienna2_fmn_hack_interface.py
@@ -1,6 +1,15 @@
 
 import subprocess
+from subprocess import CompletedProcess
 from dataclasses import dataclass
+
+from serena.utilities.ensemble_structures import Sara2SecondaryStructure, Sara2StructureList
+
+@dataclass
+class CommandResponse():
+    stdout:str
+    stderr:str
+    return_code:int
 
 @dataclass
 class RNAFoldResponse():
@@ -16,7 +25,7 @@ class Vienna2FMNInterface():
     def __init__(self) -> None:
         pass
 
-    def rnafold_fmn(self,input_sequence:str, fmn_amount:int = 200)->RNAFoldResponse:
+    def rnafold_fmn(self,input_sequence:str, fmn_amount:int = 200, fmn_delta:float = 0)-> Sara2SecondaryStructure:
         command= ["RNAfold", "--ligand", f'FMN:{fmn_amount}']
         sequence: str = ''
         structure:str = ''
@@ -33,20 +42,58 @@ class Vienna2FMNInterface():
                 energy_str = energy_str.strip(')')
                 try:
                     energy = float(energy_str)
-                except ValueError:
-                    raise Exception("not a number")
+                    energy = energy - fmn_delta
+                except ValueError as error:
+                        raise Exception(f'NOT A NUMBER, Error:{error}') 
         
-        response: RNAFoldResponse = RNAFoldResponse(sequence=sequence,
-                                                    structure=structure,
-                                                    energy=energy)
-        return response
-                        
-    def run_via_ssh():
-        pass
+        sara_struct: Sara2SecondaryStructure = Sara2SecondaryStructure(sequence=sequence,
+                                                                       structure=structure,
+                                                                       freeEnergy=energy)
 
+        return sara_struct
+
+    def rnasubopt_fmn(self, input_sequence:str, fmn_amount:int = 200):
+        struct_list_response:Sara2StructureList = Sara2StructureList()
+        sequence: str = ''
+        command= ["RNAsubopt", "--ligand", f'FMN:{fmn_amount}']
+        subopt_response = self.run_command_locally(command=command,
+                                            extra_input=input_sequence)
+        raw_result = subopt_response.stdout.split('\n')
+        found_sequence:bool = False
+        if len(raw_result) > 1:
+            sequence_raw = raw_result[0].split(' ')
+            if len(sequence_raw) > 1:
+                sequence = sequence_raw[0]
+                found_sequence = True
+        
+        if found_sequence is True:
+            #skip first one as it is the sequence
+            for index in range(1, len(raw_result)):
+                sara_struct:Sara2SecondaryStructure = Sara2SecondaryStructure()
+                raw_struct = raw_result[index].split(' ')
+                if len(raw_struct) > 1:
+                    sara_struct.sequence = sequence
+                    sara_struct.structure = raw_struct[0]
+                    try:
+                        energy = float(raw_struct[1])
+                        sara_struct.freeEnergy = energy
+                    except ValueError as error:
+                        raise Exception(f'NOT A NUMBER, Error:{error}')
+                    struct_list_response.add_structure(sara_struct)
+        
+        return struct_list_response
+                
+
+
+    def run_command_locally(self, command:str, extra_input:str ='/n')-> CommandResponse:
+        process = subprocess.run(command, input=extra_input, encoding="utf-8", capture_output=True)
+        response: CommandResponse = CommandResponse(stdout=process.stdout,
+                                                    stderr=process.stderr,
+                                                    return_code=process.returncode)
+        return response
 
 
 new_viena: Vienna2FMNInterface = Vienna2FMNInterface()
-seq = 'GCCAUCGCAUGAGGAUAUGCUCGGGUUUCCCGAGCAGAAGGCAUGUCACAAGACAUGAGGAUCACCCAUGUAGUUAAGAUGGCA'
-result = new_viena.rnafold_fmn(seq)
+seq = 'GCCAUCGCAUGAGGAUAUGCUCCCGUUUCGGGAGCAGAAGGCAUGUCAUAAGACAUGAGGAUCACCCAUGUGGUUAAGAUGGCA'
+result = new_viena.rnasubopt_fmn(seq)
 print(result)
