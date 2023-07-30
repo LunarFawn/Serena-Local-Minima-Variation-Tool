19fbbff676a82f1f15584ba1019a21d74e47ba84
Jennifer Pearl <jenpearl5@gmail.com>
2023-07-30 12:17:31 -0700

    backup and its almost fixed

--- src/serena/utilities/analysis.py
+++ src/serena/utilities/analysis.py
@@ -73,7 +73,7 @@ class LocalMinimaVariationInvestigator():
     def __init__(self) -> None:
         pass
 
-    def evaluate_lmv_for_structure_presence(self, lmv_results:ComparisonLMV, setting:SettingsAssertionLMV):          
+    def evaluate_lmv_for_structure_presence(self, lmv_data:ComparisonLMVResponse, setting:SettingsAssertionLMV):          
 
         ev_comp = lmv_results.lmv_comp.ev_normalized
         ev_comp_limit: float = 25
@@ -82,25 +82,38 @@ class LocalMinimaVariationInvestigator():
         diff_limit_mfe:float = setting.diff_limit_mfe
         diff_limit_comp:float = setting.diff_limit_comp
 
-        comp_pronounced:bool = False
-        is_on_off_switch:bool = False
-        mfe_pronounced:bool = False
-               
-
-        diff_comp:float = round(ev_mfe,2) - round(ev_comp,2)
-        if round(ev_comp,2) < round(ev_mfe,2) and diff_comp >= diff_limit_comp:
-            comp_pronounced = True
-            is_on_off_switch = True
-
-        diff_mfe = round(ev_comp,2) - round(ev_mfe,2)
-        if round(ev_mfe,2) <= round(ev_comp,2) and (diff_mfe >= diff_limit_mfe):
-            mfe_pronounced = True
+        comp_pronounced:List[bool] = []
+        is_on_off_switch:List[bool] = []
+        mfe_pronounced:List[bool] = []
         
+        for group_index in range(len(lmv_data.lmv_comps)):
+            ev_comp:float = lmv_data.lmv_comps[group_index].lmv_comp
+            ev_mfe:float = lmv_data.lmv_comps[group_index].lmv_mfe
+            
+            comp_asserted:bool = False
+            is_on_off_:bool = False
+            mfe_asserted:bool = False
+                
 
-        lmv_presence_result: LMVAssertionResult = LMVAssertionResult(bound_compare_to_unbound=ev_comp_to_mfe,
-                                                                     unbouund_pronounced=mfe_pronounced,
-                                                                     bound_pronounced=comp_pronounced,
-                                                                     is_on_off_switch=is_on_off_switch)
+            diff_comp:float = round(ev_mfe,2) - round(ev_comp,2)
+            if round(ev_comp,2) < round(ev_mfe,2) and diff_comp >= diff_limit_comp:
+                comp_asserted = True
+                is_on_off_ = True
+
+            diff_mfe = round(ev_comp,2) - round(ev_mfe,2)
+            if round(ev_mfe,2) <= round(ev_comp,2) and (diff_mfe >= diff_limit_mfe):
+                mfe_asserted = True
+            
+            comp_pronounced.append(comp_asserted)
+            mfe_pronounced.append(mfe_asserted)
+            is_on_off_switch.append(is_on_off_)
+        
+        ev_comp_to_mfe_list:List[str] = self.bound_comared_unbound_lmv(lmv_data=lmv_data)
+
+        lmv_presence_result: LMVAssertionResult = LMVAssertionResult(bound_compare_to_unbound=ev_comp_to_mfe_list,
+                                                                        unbouund_pronounced=mfe_pronounced,
+                                                                        bound_pronounced=comp_pronounced,
+                                                                        is_on_off_switch=is_on_off_switch)
 
         return lmv_presence_result
     
