1c85a7e107890b9a34d8bc129325139d0981b5d6
Jennifer Pearl <jenpearl5@gmail.com>
2023-07-30 21:54:31 -0700

    all the code is made for the investigator. need to start testing soon and do debug run

--- src/serena/utilities/investigator.py
+++ src/serena/utilities/investigator.py
@@ -35,36 +35,168 @@ class SwitchynessResult():
 
 @dataclass
 class RatioResults():
-    unbound_to_total_ratio:float = 0
-    bound_ratio: float = 0
-    bound_to_both_ratio = 0
+    unbound_to_total_ratio:float
+    bound_ratio: float
+    last_unbound_ratio: float
+    last_bound_ratio: float
+    last_both_ratio: float
+    bound_to_both_ratio: float
+    bound_to_total_ratio:float
+    both_nuc_total:float
+
+@dataclass
+class ComparisonEvalResults():
+    #last_count_unbound:float=0
+    #last_count_bound:float=0
+    #last_count_both: float = 0
+    ratios:List[RatioResults]
+    BRaise_list:List[float]
+    BUratio_list:List[float]
+    bound_total_list: List[int]
+    unbound_total_list: List[int]
+    nuc_penatly_count:int
+    first_BUratio:float
 
 #the code that makes this is not written yet...dont forget
 @dataclass
 class InvestigatorResults():
-    ratios: List[RatioResults] 
+    comparison_eval_results: List[ComparisonEvalResults] 
     comp_nuc_counts: List[ComparisonNucCounts]
-    lmv_values: List[ComparisonLMV]
+    lmv_values: ComparisonLMVResponse
     lmv_assertions: List[LMVAssertionResult]
     num_groups:int = 0
 
-@dataclass
-class AnalysisRatioResults():
-    last_count_unbound:float=0
-    last_count_bound:float=0
-    last_count_both: float = 0
-    unbound_to_total_ratio:float = 0
-    bound_ratio: float = 0
-    last_unbound_ratio = 0
-    last_bound_ratio = 0
-    last_both_ratio = 0
-    bound_to_both_ratio = 0
 
 class ComparisonInvestigator():
 
     def __init__(self) -> None:
         pass
-    
+
+    def evalulate_comparison_nucs(self, comparison_nucss:List[ComparisonNucCounts])->ComparisonEvalResults:
+
+        BRaise_list:List[float] = []
+        BUratio_list:List[float] = []
+        bound_total_list: List[int] = []
+        unbound_total_list: List[int] = []
+        ratios:List[RatioResults] = []
+        nuc_penatly_count:int = 0
+        for group_index in range(len(comparison_nucss)):
+            last_index:int = 0
+            if group_index > 0:
+                last_index = group_index -1
+            unbound:float = comparison_nucss[group_index].unbound_count
+            last_unbound:float = comparison_nucss[last_index].unbound_count
+            
+            bound:float = comparison_nucss[group_index].bound_count
+            last_bound:float = comparison_nucss[last_index].bound_count
+
+            both_nuc:float = comparison_nucss[group_index].both_count
+            last_both:float = comparison_nucss[last_index].both_count
+
+            dot_nuc:float = comparison_nucss[group_index].dot_count
+
+            nuc_count:int = comparison_nucss[last_index].num_nucs
+
+            unbound_to_total_ratio:float = 0
+            bound_to_total_ratio:float = 0
+            both_nuc_total:float = 0
+            bound_ratio: float = 0
+            last_unbound_ratio = 0
+            last_bound_ratio = 0
+            last_both_ratio = 0
+            bound_to_both_ratio = 0
+            try:
+                last_unbound_ratio = last_unbound/unbound 
+            except:
+                pass
+            
+            try:
+                bound_ratio = bound/unbound
+            except:
+                pass
+
+            try:
+
+                if bound_hold != -1:
+                    #do normal                    
+                    if bound_hold < last_bound: 
+                        if bound_hold == 0:
+                            bound_hold = 1                   
+                        last_bound_ratio = bound/bound_hold 
+                    else:
+                        last_bound_ratio = bound/last_bound 
+                else:
+                    last_bound_ratio = bound/last_bound
+
+                if bound > last_bound:
+                    #its getting bigger so record that
+                    bound_hold = last_bound   
+                else:
+                    bound_hold = -1    
+            except:
+                pass
+            
+            #added to address the ones with 0 in the first group
+            if group_index > 0:
+                if BRaise_list[group_index-1] == 0 and bound > 0:
+                    last_bound_ratio = bound
+
+
+            try:
+                last_both_ratio = both_nuc/last_both 
+            except:
+                pass
+            
+            try:
+                bound_to_both_ratio = bound/(both_nuc - unbound)
+            except:
+                pass
+
+
+
+            unbound_to_total_ratio = unbound/nuc_count
+            bound_to_total_ratio = bound/nuc_count
+            both_nuc_total= both_nuc/nuc_count
+            dot_nuc_total= dot_nuc/nuc_count
+
+            bound_total_list.append(bound_to_total_ratio)
+            unbound_total_list.append(unbound_to_total_ratio)  
+            
+            #now round teh data to make it more managable
+            last_unbound_ratio = round(last_unbound_ratio,2)
+            last_bound_ratio = round(last_bound_ratio,2)
+            unbound_to_total_ratio = round(unbound_to_total_ratio,2)
+            bound_ratio = round(bound_ratio,2)
+            bound_stats: str = f'BURatio:{round(bound_ratio,2)},both_Raise:{round(last_both_ratio,2)} BRaise:{round(last_bound_ratio,2)}, UDrop:{round(last_unbound_ratio,2)},BothTotal:{round(both_nuc_total,2)}, BoundTotal:{round(bound_to_total_ratio,2)}, UTotal:{round(unbound_to_total_ratio,2)}, bound_both:{round(bound_to_both_ratio,2)} B:{bound}, U:{unbound}. both:{both_nuc}'
+
+
+            #this is only for the fist kcal group
+            if group_index == 0:
+                nuc_penatly_count = bound
+                first_BUratio = round(bound_ratio,2)
+            
+            BUratio_list.append(round(bound_ratio,2))
+            BRaise_list.append(round(bound,2))
+
+            ratio_results:RatioResults = RatioResults(unbound_to_total_ratio=unbound_to_total_ratio,
+                                                      bound_ratio=bound_ratio,
+                                                      last_unbound_ratio=last_unbound_ratio,
+                                                      last_bound_ratio=last_bound_ratio,
+                                                      last_both_ratio=last_both_ratio,
+                                                      bound_to_both_ratio=bound_to_both_ratio,
+                                                      bound_to_total_ratio=bound_to_total_ratio,
+                                                      both_nuc_total=both_nuc_total
+                                                      )
+            ratios.append(ratio_results)
+
+        comparison_eval_results: ComparisonEvalResults = ComparisonEvalResults(ratios=ratios,
+                                                                               BRaise_list=BRaise_list,
+                                                                               BUratio_list=BUratio_list,
+                                                                               bound_total_list=bound_total_list,
+                                                                               unbound_total_list=unbound_total_list,
+                                                                               nuc_penatly_count=nuc_penatly_count,
+                                                                               first_BUratio=first_BUratio)
+        return comparison_eval_results
     
 
 
