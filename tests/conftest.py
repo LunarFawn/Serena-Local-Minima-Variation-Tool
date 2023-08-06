e4284ef5b10d4bca8ed0c1fcd1458c6250dbf5ae
Jennifer Pearl <jenpearl5@gmail.com>
2023-08-06 10:01:32 -0700

    finished adding comparison struct unit tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,4 @@
+from typing import List
 import pytest
 
 from serena.utilities.ensemble_structures import (Sara2SecondaryStructure, 
@@ -42,6 +43,62 @@ def secondary_structure_2():
                                    freeEnergy=free_energy,
                                    stackEnergy=stack_energgy)
 
+@pytest.fixture()
+def secondary_structure_3():
+    """
+    Returns a initialed secondary structure object
+    """
+    sequence:str = 'GCCAUA'
+    structure: str = '((.)))'
+    free_energy:float = -30
+    stack_energgy:float = -10
+    return Sara2SecondaryStructure(sequence=sequence,
+                                   structure=structure,
+                                   freeEnergy=free_energy,
+                                   stackEnergy=stack_energgy)
+
+@pytest.fixture()
+def secondary_structure_3_1():
+    """
+    Returns a initialed secondary structure object
+    """
+    sequence:str = 'GCCAUA'
+    structure: str = '((..))'
+    free_energy:float = -30
+    stack_energgy:float = -10
+    return Sara2SecondaryStructure(sequence=sequence,
+                                   structure=structure,
+                                   freeEnergy=free_energy,
+                                   stackEnergy=stack_energgy)
+
+@pytest.fixture()
+def secondary_structure_4():
+    """
+    Returns a initialed secondary structure object
+    """
+    sequence:str = 'GCCAUA'
+    structure: str = '..().)'
+    free_energy:float = -50
+    stack_energgy:float = -20
+    return Sara2SecondaryStructure(sequence=sequence,
+                                   structure=structure,
+                                   freeEnergy=free_energy,
+                                   stackEnergy=stack_energgy)
+
+@pytest.fixture()
+def secondary_structure_5():
+    """
+    Returns a initialed secondary structure object
+    """
+    sequence:str = 'GCCAUA'
+    structure: str = '(...))'
+    free_energy:float = -40
+    stack_energgy:float = -30
+    return Sara2SecondaryStructure(sequence=sequence,
+                                   structure=structure,
+                                   freeEnergy=free_energy,
+                                   stackEnergy=stack_energgy)
+
 @pytest.fixture
 def empty_secondary_structure_list():
     """
@@ -73,3 +130,32 @@ def comparison_nuc_count():
                                 both_count=3,
                                 dot_count=4,
                                 num_nucs=5)
+
+@pytest.fixture
+def comparison_nuc_count_2():
+    """
+    Returns populated comparions nuc pair dataclass
+    """
+    return ComparisonNucCounts(bound_count = 2,
+                                unbound_count=3,
+                                both_count=4,
+                                dot_count=5,
+                                num_nucs=5)                            
+
+@pytest.fixture
+def comparison_nuc_result(comparison_nuc_count:ComparisonNucCounts, comparison_nuc_count_2:ComparisonNucCounts):
+    """
+    Returns a comparison nuc result
+    """
+    comp_list:List[ComparisonNucResults] = []
+    comp_list.append(comparison_nuc_count)
+    comp_list.append(comparison_nuc_count_2)
+    return ComparisonNucResults(comparison_nuc_counts=comp_list)
+
+@pytest.fixture
+def comparison_result(secondary_structure_3:Sara2SecondaryStructure, comparison_nuc_count:ComparisonNucCounts):
+    """
+    Returns a comparison result (not the list one)
+    """
+    return ComparisonResult(comp_struct=secondary_structure_3,
+                            comp_counts=comparison_nuc_count)
