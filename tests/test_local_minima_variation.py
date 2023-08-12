1d93ef0a5ba1dcc849f0e1a6d7c2947007a242c6
Jennifer [C] Pearl <pearljen@amazon.com>
2023-08-11 19:18:41 -0700

    added new lmv unti test file

---
+++ tests/test_local_minima_variation.py
@@ -0,0 +1,8 @@
+import pytest
+
+from serena.utilities.ensemble_structures import (Sara2SecondaryStructure, 
+                                        Sara2StructureList, 
+                                        )
+
+from serena.utilities.local_minima_variation import ComparisonLMV, ComparisonLMVResponse
+
