b6e8f5470613bc86e2a4d75a6f1dfab157e3458b
Jennifer Pearl <jenpearl5@gmail.com>
2023-08-13 13:44:36 -0700

    changing structure ready for PR

---
+++ src/serena/ensemble_local_minima_variation.py
@@ -0,0 +1,3 @@
+"""
+This file is the entry point to get lmv results for an ensemble
+"""
---
+++ src/serena/ensemble_variation.py
@@ -0,0 +1,5 @@
+"""
+This is the file to just get plain old ensemble variation for a list
+of structures that make up an ensemble
+"""
+
