7a198130ba9524a9fe7324713c540bd02e5db1ea
Jennifer Pearl <jenpearl5@gmail.com>
2023-08-13 19:59:03 -0700

    backup

--- src/serena/ensemble_variation.py
+++ src/serena/ensemble_variation.py
@@ -4,7 +4,6 @@ of structures that make up an ensemble
 """
 
 
-import attrs
 from typing import List
 
 from serena.utilities.ensemble_structures import Sara2SecondaryStructure, Sara2StructureList
@@ -12,12 +11,12 @@ from serena.utilities.ensemble_variation import EV, EnsembleVariation
 from serena.utilities.weighted_structures import WeightedEnsembleResult
 
 
-
 class RunEnsembleVariation():
-    
+    """
+    Class to get the ensemble variation
+    """
     def ev_from_structures_list(self, structures_list:Sara2StructureList, mfe_structure:Sara2SecondaryStructure):
         ensemble_variation:EnsembleVariation = EnsembleVariation()
         ev:EV = ensemble_variation.ensemble_variation_algorithm(kcal_group_structures_list=structures_list,
                                                         ref_structure=mfe_structure)
-        
         return ev.ev_normalized
