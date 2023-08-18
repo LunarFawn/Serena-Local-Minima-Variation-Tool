330339990180cc371a2c7fbbebfe70ccc7aad80b
Jennifer Pearl <jenpearl5@gmail.com>
2023-08-17 18:35:27 -0700

    backup

---
+++ tests/test_nupack4_interface.py
@@ -0,0 +1,10 @@
+from serena.interfaces.nupack4_0_28_wsl2_interface import NUPACK4Interface, NupackSettings
+
+
+
+
+
+
+def test_nupack_4():
+    nupack4: NUPACK4Interface = NUPACK4Interface()
+    
